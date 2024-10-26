adde2f0a1a6f0af3d31f92eb70f6b580eada8210
sofialiles55 <sofia.liles.5@gmail.com>
2024-10-26 15:10:07 +0200

    Player script

---
+++ Arkanoid_SofiaLiles/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float playerVelocity;
+    private Vector3 playerPosition;
+    [SerializeField] private float boundary; // Use SerializeField for private access
+
+    // Use this for initialization 
+    void Start()
+    {
+        // Get the initial position of the game object 
+        playerPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        // Move with mouse
+        float mouseX = Input.mousePosition.x;
+        // Convert mouse position to world position
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(mouseX, 0, Camera.main.nearClipPlane));
+
+        // Update player position based on mouse position
+        playerPosition.x = mouseWorldPosition.x;
+
+        // Apply boundaries
+        if (playerPosition.x < -boundary)
+        {
+            playerPosition.x = -boundary;
+        }
+        if (playerPosition.x > boundary)
+        {
+            playerPosition.x = boundary;
+        }
+
+        // Update the game object transform 
+        transform.position = new Vector3(playerPosition.x, transform.position.y, transform.position.z);
+
+        // Leave the game 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
